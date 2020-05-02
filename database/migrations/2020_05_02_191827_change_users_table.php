8cbd1e1d5a310bccb9eee406ce2dbd9d2e8f32de
Pablo Henrique Ribeiro <ribeiro.pablo.henrique@gmail.com>
2020-05-02 17:12:05 -0300

    Creating database table migrations

---
+++ database/migrations/2020_05_02_191827_change_users_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('email');
+            $table->dropColumn('email_verified_at');
+            $table->dropColumn('password');
+            $table->dropColumn('remember_token');
+
+            $table->string('phone_number');
+            $table->string('type');
+            $table->string('picture')->nullable();
+            $table->foreignId('address_id');
+
+            $table->foreign('address_id')->references('id')->on('addresses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+
+            $table->dropForeign('address_id');
+            $table->dropColumn('address_id');
+            $table->dropColumn('phone_number');
+            $table->dropColumn('type');
+            $table->dropColumn('picture');
+        });
+    }
+}
--- database/migrations/2020_05_02_200538_create_request_offers_table.php
+++ database/migrations/2020_05_02_200538_create_request_offers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateRequestOffersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-            Schema::create('users', function (Blueprint $table) {
+        Schema::create('request_offers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('help_request_id');
+            $table->foreignId('user_helper');
             $table->timestamps();
+
+            $table->foreign('help_request_id')->references('id')->on('help_requests');
+            $table->foreign('user_helper')->references('id')->on('users');
         });
     }
 
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('request_offers');
     }
 }
