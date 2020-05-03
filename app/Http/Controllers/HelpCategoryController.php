b9bb311ef187ff6a89919825cc7a09d925551658
Pablo Henrique Ribeiro <ribeiro.pablo.henrique@gmail.com>
2020-05-03 11:03:07 -0300

    Creating a database seeder to store some basic categories. Creating a method to retrieve categories.

--- app/Http/Controllers/HelpCategoryController.php
+++ app/Http/Controllers/HelpCategoryController.php
@@ -4,10 +4,14 @@ declare (strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Models\HelpCategory;
+
 class HelpCategoryController extends Controller
 {
     public function index()
     {
+        $categories = HelpCategory::get();
 
+        return response()->json(['message' => 'Categories retrieved successfully.', 'categories' => $categories]);
     }
 }
